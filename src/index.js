d97945f03d2ceb5186c55d5ebb5a78fe73d198e9
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-04-16 00:06:23 -0700

    update

--- src/index.js
+++ src/index.js
@@ -3,30 +3,35 @@ import KeyPressMapCallBack from "./util/KeyPressMapCallBack.js"
 import {KEYS, GameSettings} from "./settings.js";
 import Game from "./Pong/Game";
 
-const game = new Game(GameSettings);
+document.addEventListener("DOMContentLoaded", function(event) {
 
-const keyMapCallBack = new KeyPressMapCallBack(new Map([
-    [ KEYS.p1Up     , () => game.player1MoveUp()   ],
-    [ KEYS.p1Down   , () => game.player1MoveDown() ],
-    [ KEYS.p2Up     , () => game.player2MoveUp()   ],
-    [ KEYS.p2Down   , () => game.player2MoveDown() ],
-    [ KEYS.gameStart, () => game.startOrPause()    ],
-    [ KEYS.addBall  , () => game.addBall()         ],
-    [ KEYS.reset    , () => game.reset()           ]
-]));
+    const game = new Game(GameSettings);
 
-document.body.addEventListener("keydown", function (e) {
-    const isKeyIsMapped = keyMapCallBack.press(e.code);
-    if(isKeyIsMapped){
-        event.preventDefault();
-    }
+    const keyMapCallBack = new KeyPressMapCallBack(new Map([
+        [ KEYS.p1Up     , () => game.player1MoveUp()   ],
+        [ KEYS.p1Down   , () => game.player1MoveDown() ],
+        [ KEYS.p2Up     , () => game.player2MoveUp()   ],
+        [ KEYS.p2Down   , () => game.player2MoveDown() ],
+        [ KEYS.gameStart, () => game.startOrPause()    ],
+        [ KEYS.addBall  , () => game.addBall()         ],
+        [ KEYS.reset    , () => game.reset()           ]
+    ]));
+
+    document.body.addEventListener("keydown", function (e) {
+        const isKeyIsMapped = keyMapCallBack.press(e.code);
+        if(isKeyIsMapped){
+            event.preventDefault();
+        }
+    });
+    document.body.addEventListener("keyup", function (e) {
+        keyMapCallBack.release(e.code);
+    });
+
+
+    (function gameLoop() {
+        game.gameloop();
+        requestAnimationFrame(gameLoop);
+    })();
+
+
 });
-document.body.addEventListener("keyup", function (e) {
-    keyMapCallBack.release(e.code);
-});
-
-
-(function gameLoop() {
-    game.gameloop();
-    requestAnimationFrame(gameLoop);
-})();
