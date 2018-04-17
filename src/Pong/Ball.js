872b1cd987981fa36c5dd4642a70cda5f58d4cb2
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-04-16 19:21:04 -0700

    Adds More Paddle Collision

--- src/Pong/Ball.js
+++ src/Pong/Ball.js
@@ -105,25 +105,25 @@ export class Ball{
     checkforPaddleCollision(ballBox, paddle1Box, paddle2Box){
 
         const hitP1PadBySide  = ballBox.x <= paddle1Box.x2 && ballBox.y2 >= paddle1Box.y && ballBox.y  <= paddle1Box.y2 && this.vx < 0;
-        // const hitP1FromTop    = ballBox.x <= paddle1Box.x2 && ballBox.x2 >= paddle1Box.x && ballBox.y  <  paddle1Box.y  && ballBox.y2 >= paddle1Box.y  && ballBox.cy <= paddle1Box.cy && this.vy > 0;
-        // const hitP1FromBotton = ballBox.x <= paddle1Box.x2 && ballBox.x2 >= paddle1Box.x && ballBox.y2 >  paddle1Box.y2 && ballBox.y  <= paddle1Box.y2 && ballBox.cy >= paddle1Box.cy && this.vy < 0;
+        const hitP1FromTop    = ballBox.x <= paddle1Box.x2 && ballBox.x2 >= paddle1Box.x && ballBox.y  <  paddle1Box.y  && ballBox.y2 >= paddle1Box.y  && ballBox.cy <= paddle1Box.cy && this.vy > 0;
+        const hitP1FromBotton = ballBox.x <= paddle1Box.x2 && ballBox.x2 >= paddle1Box.x && ballBox.y2 >  paddle1Box.y2 && ballBox.y  <= paddle1Box.y2 && ballBox.cy >= paddle1Box.cy && this.vy < 0;
 
         const hitP2PadBySide  = ballBox.x2 >= paddle2Box.x && ballBox.y2 >= paddle2Box.y  && ballBox.y  <= paddle2Box.y2 && this.vx > 0;
-        // const hitP2FromTop    = ballBox.x2 >= paddle2Box.x && ballBox.x  >= paddle2Box.x2 && ballBox.y  <  paddle1Box.y  && ballBox.y2 >= paddle1Box.y  && ballBox.cy <= paddle1Box.cy && this.vy > 0;
-        // const hitP2FromBotton = ballBox.x2 >= paddle2Box.x && ballBox.x  >= paddle1Box.x2 && ballBox.y2 >  paddle2Box.y2 && ballBox.y  <= paddle2Box.y2 && ballBox.cy >= paddle2Box.cy && this.vy < 0;
+        const hitP2FromTop    = ballBox.x2 >= paddle2Box.x && ballBox.x  >= paddle2Box.x2 && ballBox.y  <  paddle1Box.y  && ballBox.y2 >= paddle1Box.y  && ballBox.cy <= paddle1Box.cy && this.vy > 0;
+        const hitP2FromBotton = ballBox.x2 >= paddle2Box.x && ballBox.x  >= paddle1Box.x2 && ballBox.y2 >  paddle2Box.y2 && ballBox.y  <= paddle2Box.y2 && ballBox.cy >= paddle2Box.cy && this.vy < 0;
 
-        // if(hitP1FromTop || hitP2FromTop){
-        //     this.vy *= -1;
-        //     this.collisionDetected();
-        //     console.log("HitPaddle! Front Top");
-        //     return;
-        // }
-        // if(hitP1FromBotton || hitP2FromBotton){
-        //     this.vy *= -1;
-        //     this.collisionDetected();
-        //     console.log("HitPaddle! Front Botton");
-        //     return;
-        // }
+        if(hitP1FromTop || hitP2FromTop){
+            this.vy *= -1;
+            this.collisionDetected();
+            console.log("HitPaddle! Front Top");
+            return;
+        }
+        if(hitP1FromBotton || hitP2FromBotton){
+            this.vy *= -1;
+            this.collisionDetected();
+            console.log("HitPaddle! Front Botton");
+            return;
+        }
         if(hitP1PadBySide || hitP2PadBySide){
             this.vx *= -1;
             this.direction *= -1;
